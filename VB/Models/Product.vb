6dd06a9129252cd87e90b5c8ed31d5dcd9169e40
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 18:32:22 +0400

    Change example status to out of maintenance

--- VB/Models/Product.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.ComponentModel.DataAnnotations
-
-Public Class Product
-	Public Property ProductID() As Integer
-	<Required(ErrorMessage := "Product Name is required")>
-	Public Property ProductName() As String
-	Public Property UnitPrice() As Decimal?
-	Public Property UnitsOnOrder() As Short?
-End Class
--- VB/Views/Shared/_LoginPartial.vbhtml
+++
@@ -1,4 +0,0 @@
-﻿<ul class="nav navbar-nav navbar-right">
-    <li>@Html.ActionLink("Register", "Index", "Home", Nothing, New With {.id = "registerLink"})</li>
-    <li>@Html.ActionLink("Log in", "Index", "Home", Nothing, New With {.id = "loginLink"})</li>
-</ul>
